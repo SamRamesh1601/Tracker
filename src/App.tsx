9d7586a4bd89250912dfd042a8b4d4490ca77a62
SamRamesh1601 <josarun1601@gmail.com>
2025-11-08 01:45:52 +0530

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import Sidebar from "./views/Sidebar";
+
 export default function App() {
   return(
-    <div>App</div>
+    <div className="body">
+
+      <Sidebar />
+
+      <div className="Route-Container">
+
+        <div className="Header-Container">
+                Header Container
+            </div>
+        <div className="Main-Container">
+                
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+          <div className="First-Container">First</div>
+          <div className="Second-Container">Second</div>
+
+        </div>
+
+
+
+
+      </div>
+
+    </div>
   )
 }
